749bf8a773c13d0b6021e1c7f2780fee9c510dba
reggordon <reg@reggordon.com>
2015-09-10 13:57:18 +0000

    added controllers and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users do
+    resource :profile
+  end
   resources:contacts
-  get '/login' => 'pages#login'
+
   get '/about' => 'pages#about'
   get '/stuff' => 'pages#stuff'
   root'pages#home'
