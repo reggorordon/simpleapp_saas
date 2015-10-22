5319ac005b99933afb9b778d22b669754ac53e68
reggordon <reg@reggordon.com>
2015-10-22 06:05:48 +0000

    showing profile on seperate page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,9 +4,8 @@ class ProfilesController < ApplicationController
     @user = User.find( params[:user_id] )
     @profile = @user.build_profile
   end
-  
-  def create
-    @user= User.find( params[:user_id])
+  def create 
+    @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
     if @profile.save
       flash[:success] = "Profile Updated!"
@@ -14,13 +13,9 @@ class ProfilesController < ApplicationController
     else
       render action: :new
     end
-    
-    private
+  end
+  private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
-    
-  end
-  
-  
 end
